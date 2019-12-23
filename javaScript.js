d8121d8b0e102dc00dba63ed5e4e66fbe4455cb8
宋畅 <44915632+meichangchang@users.noreply.github.com>
2019-12-23 20:11:02 +0800

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,44 +1,34 @@
 var pic = document.getElementById("pic");
 var imgList = pic.getElementsByTagName("img");
-    // var txt = document.getElementById("txt");
-    // var comments = ["差","一般","中等","还行","好"];
-    var flag = true;
-    for(var i = 0;i<imgList.length;i++){
-        imgList[i].onmouseover = function(){
-            if(flag==true){
-                var index = this.getAttribute("title");
-                if(index>2){
-                    var imgSrc = "./images/star2.png";
-                }
-                else{
-                    var imgSrc = "./images/star2.png";
-                }
-                
-                for(var j=0;j<index;j++){
+var flag = true;
+for(var i = 0;i<imgList.length;i++){
+    imgList[i].onmouseover = function(){
+        if(flag==true){
+            var index = this.getAttribute("title");          
+            var imgSrc = "./images/star2.png";
+            for(var j=0;j<index;j++){
                     imgList[j].src = imgSrc;
-                }
-                // txt.innerHTML = comments[index-1];
-            }
-            else{
-                return;
             }
         }
-        imgList[i].onmouseout = function(){
-            if(flag==true){
-                var index = this.getAttribute("title");
-                for(var x=0;x<index;x++){
-                    imgList[x].src = "./images/star0.png";
-                }
-                // txt.innerHTML = " ";
-            }
-            else{
-                return;
-            }
-        }
-        imgList[i].onclick=function(){
-            flag = !flag;
+        else{
+            return;
         }
     }
+     imgList[i].onmouseout = function(){
+        if(flag==true){
+            var index = this.getAttribute("title");
+            for(var x=0;x<index;x++){
+                imgList[x].src = "./images/star0.png";
+            }
+        }
+        else{
+            return;
+        }
+    }
+    imgList[i].onclick=function(){
+        flag = !flag;
+    }
+}
 var btns = document.getElementsByClassName("btn")
 var contents = document.getElementsByClassName("content")
 for(var i=0;i<btns.length;i++){
@@ -60,9 +50,9 @@ var isMoving=false;
 setInterval(start,280);
 function start() {
     ind++;
-    animate(word,{right:0+10*ind},function(){
+    animate(word,{right:-270+10*ind},function(){
     	if(ind===90){
-    		word.style.right="0px";
+    		word.style.right="-270px";
     		ind=1;
     	}
     });
@@ -121,17 +111,15 @@ function next(){
 }
 var timer=setInterval(next,3000);
 window.onscroll = function(){
-  var distance = document.documentElement.scrollTop ; 
-  if( distance >= 200 ) { //当距离顶部超过300px时，显示图片
-    document.getElementById('to_top').style.display = "";
-  } else { //距离顶部小于300px，隐藏图片
-    document.getElementById('to_top').style.display = "none";
-  }
-
-  var toTop = document.getElementById("to_top"); //获取图片所在的div
-
-  toTop.onclick = function(){ //点击图片时触发的点击事件
-    document.documentElement.scrollTop = document.body.scrollTop = 0; //页面移动到顶部
-  }
+    var distance = document.documentElement.scrollTop ; 
+    if( distance >= 200 ) { 
+        document.getElementById('to_top').style.display = "";
+    } 
+    else { 
+        document.getElementById('to_top').style.display = "none";
+    }
+    var toTop = document.getElementById("to_top"); 
+    toTop.onclick = function(){ 
+        document.documentElement.scrollTop = document.body.scrollTop = 0; 
+    }
 }
-
